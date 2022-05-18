a702417abb258a6146f0ef10a4fb8ce4104f5890
Rodrigo <rogomezpalacio@protonmail.com>
2022-05-18 15:00:54 -0500

    Commit example app

---
+++ example/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+// eslint-disable-next-line no-unused-vars
+import OneSignal from '@onesignal/onesignal-vue3';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    this.$OneSignal.showSlidedownPrompt();
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
