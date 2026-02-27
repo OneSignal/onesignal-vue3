8b4807b764d64beee68c80b5fb75f753b18129d4
OneSignal <noreply@onesignal.com>
2026-02-27 16:29:32 +0000

    build: sync with web-shim-codegen v3.0.8

--- index.ts
+++ index.ts
@@ -225,6 +225,10 @@ export interface IOSNotificationActionButton {
   readonly launchURL?: string;
 }
 
+export interface IDisplayableOSNotification extends IOSNotification {
+  display(): void;
+}
+
 export interface NotificationClickResult {
   readonly actionId?: string;
   readonly url?: string;
@@ -239,7 +243,7 @@ export type NotificationEventTypeMap = {
 };
 
 export interface NotificationForegroundWillDisplayEvent {
-  readonly notification: IOSNotification;
+  readonly notification: IDisplayableOSNotification;
   preventDefault(): void;
 }
 
